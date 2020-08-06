41f6049222d4059ab3cdbef6e029cf104695f92a
Smorkster <smorkster@gmail.com>
2020-08-06 09:32:42 +0200

    Add files via upload

---
+++ Modules/FileOps.psm1
@@ -0,0 +1,155 @@
+# A module for functions operating on files
+# Use this to import module:
+# Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
+
+# Creates a file to use for input from user, then returns its content.
+# If file exists, its content is replaced, otherwise a new file is created.
+# DefaultText is entered as initial content.
+# Returns the files content, with DefaultText removed.
+function GetUserInput
+{
+	param ( $DefaultText )
+	$InputFilePath = "$RootDir\Input\$env:USERNAME\$CallingScript.txt"
+	if ( Test-Path -Path $InputFilePath )
+	{
+		Clear-Content $InputFilePath
+	}
+	else
+	{
+		New-Item -Path $InputFilePath -ItemType File -Force | Out-Null
+	}
+
+	if ( $DefaultText )
+	{
+		Set-Content $InputFilePath $DefaultText
+	}
+	Start-Process notepad $InputFilePath -Wait
+
+	return Get-Content $InputFilePath | where { $_ -notlike $DefaultText }
+}
+
+# Reads input from console given by Ctrl+V
+# Input is returned as an array, split according to Split
+function GetConsolePasteInput
+{
+	param ( [switch] $Folders )
+	$Quit = New-Object -ComObject wscript.shell
+
+	$Users1 = @()
+	do
+	{
+		if ( $Folders )
+		{ $Input = ( Read-Host ).Split( "`n""`n""`n""`r`n"","";" ) }
+		else
+		{ $Input = ( Read-Host ).Split( "`n"" `n""`n ""`r`n"","";"" "":""-""_""/""\""."" `r`n""`r`n "", ""; "": ""- ""_ ""/ ""\ "". ", [System.StringSplitOptions]::RemoveEmptyEntries ) }
+
+		if ( $input -ne '' )
+		{
+			$Users1 += $input
+		}
+		else
+		{
+			$Quit.SendKeys( "Done" )
+			$Quit.SendKeys( "~" )
+		}
+	} until ( $input -eq "Done" )
+	$Users2 = $Users1 -ne "Done"
+
+	return $Users2
+}
+
+# Writes given output to file from script or scoreboard.
+# Returns filepath to outputfile.
+function WriteOutput
+{
+	param ( $FileNameAddition, $Output, $FileExtension = "txt", [switch] $Scoreboard )
+	if ( $Scoreboard ) { $Folder = "Scoreboard" } else { $Folder = $env:USERNAME }
+
+	$OutputFilePath = "$RootDir\Output\$Folder\$( if ( $FileNameAddition ) { "$FileNameAddition " } )$CallingScript, $( Get-Date -Format "yyyy-MM-dd HH.mm.ss").$FileExtension"
+	if ( -not ( Test-Path $OutputFilePath ) ) { New-Item -Path $OutputFilePath -ItemType File -Force | Out-Null }
+	Set-Content -Path $OutputFilePath -Value ( $Output )
+	return $OutputFilePath
+}
+
+# Writes log from running script.
+# Each row is preceded by default logdata, followed by logtext from script.
+function WriteLog
+{
+	param ( $LogText )
+	$LogFilePath = "$RootDir\Logs\$( [datetime]::Now.Year )\$( [datetime]::Now.Month )\$CallingScript - log.txt" # Create filepath for file
+	if ( -not ( Test-Path $LogFilePath ) ) { New-Item -Path $LogFilePath -ItemType File -Force | Out-Null } # If file does not exist, create it
+	Add-Content -Path $LogFilePath -Value ( $nudate + " " + $env:USERNAME + " [" + $env:USERDOMAIN + "] => " + $LogText )
+}
+
+# Writes errorlog from running script.
+# Each row is preceded by default logdata, followed by errortext from script.
+# Returns filepath to file.
+function WriteErrorlog
+{
+	param ( $LogText )
+	$ErrorLogFilePath = "$RootDir\ErrorLogs\$( [datetime]::Now.Year )\$( [datetime]::Now.Month )\$CallingScript - Errorlog $( Get-Date -Format 'yyyyMMddHHmmss' ).txt"
+	if ( -not ( Test-Path $ErrorLogFilePath ) ) { New-Item -Path $ErrorLogFilePath -ItemType File -Force | Out-Null } # If file does not exist, create it
+	Add-Content -Path $ErrorLogFilePath -Value ( ( Get-Date -Format "yyyy-MM-dd HH:mm:ss" ) + " " + $env:USERNAME + " [" + $env:USERDOMAIN + "] => " + $LogText )
+	return $ErrorLogFilePath
+}
+
+# Display a messagebox with given text, title, button and icon
+# Returns button pressed.
+function ShowMessageBox
+{
+	param (
+		$Text,
+		$Title = "",
+		$Button = "OK",
+		$Icon = "Info"
+	)
+	return [System.Windows.MessageBox]::Show( "$Text", "$Title", "$Button", "$Icon" )
+}
+
+# Displays message in console that the script can be closed.
+function EndScript
+{
+	$dummy = Read-Host "`nPress Enter to exit"
+	if ( $dummy -ne "" )
+	{ Add-Content -Path "$RootDir\Logs\DummyQuitting.txt" -Value "$nudate $env:USERNAME $CallingScript - $dummy" }
+}
+
+# Creates a WPF-window, based on XAML-file with the same name as the script calling.
+# Returns window and an array with all named controls in window.
+function CreateWindow
+{
+	Add-Type -AssemblyName PresentationFramework
+	$XamlFile = "$RootDir\Gui\$CallingScript.xaml"
+	$inputXML = Get-Content $XamlFile -Raw
+	$inputXML = $inputXML -replace "x:N", 'N' -replace '^<Win.*', '<Window'
+	[XML]$XAML = $inputXML
+
+	$reader = ( New-Object System.Xml.XmlNodeReader $Xaml )
+	try
+	{
+		$Window = [Windows.Markup.XamlReader]::Load( $reader )
+	}
+	catch
+	{
+		Write-Host $_
+		Read-Host
+		throw
+	}
+	$vars = @()
+	$xaml.SelectNodes( "//*[@Name]" ) | foreach {
+		$vars += $_.Name
+	}
+
+	return $Window, $vars
+}
+
+# Current date and time
+$nudate = Get-Date -Format "yyyy-MM-dd HH:mm"
+# Root directory for scriptmenu
+$RootDir = ( Get-Item $PSCommandPath ).Directory.Parent.FullName
+# Name of the script calling
+$CallingScript = ( Get-Item $MyInvocation.PSCommandPath ).BaseName
+# Set title for consolewindow to scriptname
+$Host.UI.RawUI.WindowTitle = "Script: $( ( ( Get-Item $MyInvocation.PSCommandPath ).FullName -split "Skript" )[1] )"
+
+Export-ModuleMember -Function *
---
+++ Modules/SysManOps.psm1
@@ -0,0 +1,35 @@
+# A module for functions operating with SysMan 
+# Use this to import module:
+# Import-Module "$( $args[0] )\Modules\SysManOps.psm1" -Force
+
+######################################################################################################
+# Changes Office version, from Office ProfessionalPlus, to ProPlus (that contains Outlook and Skype)
+function ChangeOfficeInstallation
+{
+	param( $ComputerName, $OldVersion, $NewVersion )
+
+	$ServerUrl = "http://sysman.sll.se/sysman"
+	$Application = ( Get-ADObject -LDAPFilter "(&(objectclass=group)(name=$OldVersion))" | select -ExpandProperty name ).Replace( "_I", "" )
+	$NewApplication = ( Get-ADObject -LDAPFilter "(&(objectclass=group)(name=$NewVersion))" | select -ExpandProperty name ).Replace( "_I", "" )
+
+	$SystemID = ( ( Invoke-WebRequest -Uri "$( $ServerUrl )/api/System?name=$( $Application )" -UseDefaultCredentials -ContentType "application/json" ) | ConvertFrom-Json).ID
+	$NewSystemID = ( ( Invoke-WebRequest -Uri "$( $ServerUrl )/api/System?name=$( $NewApplication )" -UseDefaultCredentials -ContentType "application/json" ) | ConvertFrom-Json).ID
+	$ComputerID = ( ( Invoke-WebRequest -Uri "$( $ServerUrl )/api/Client?name=$( $ComputerName )" -UseDefaultCredentials -ContentType "application/json" ) | ConvertFrom-Json).ID
+
+	$ComputerCollection = @( "$ComputerID" )
+	$SystemCollection = @( "$SystemID" )
+	$RequestInput = @{ "Targets" = $ComputerCollection; "Systems" = $SystemCollection }
+
+	$StateResult = ( Invoke-WebRequest -Uri "$( $ServerUrl )/api/application/Uninstall" -Method Post -Body ( ConvertTo-Json -InputObject $RequestInput ) -UseDefaultCredentials -ContentType "application/json" -ErrorAction Stop )
+
+	$SystemCollection = @( "$NewSystemID" )
+	$RequestInput = @{ "Targets"=$ComputerCollection; "Softwares"=$SystemCollection }
+
+	$StateResult = ( Invoke-WebRequest -Uri "$( $ServerUrl )/api/application/Install" -Method Post -Body ( ConvertTo-Json -InputObject $RequestInput ) -UseDefaultCredentials -ContentType "application/json" -ErrorAction Stop )
+}
+
+$nudate = Get-Date -Format "yyyy-MM-dd HH:mm"
+$RootDir = ( Get-Item $PSCommandPath ).Directory.Parent.FullName
+$CallingScript = ( Get-Item $MyInvocation.PSCommandPath ).BaseName
+
+Export-ModuleMember -Function *
