884a7171e63912a41ee4078f62f421c3bf6ef164
Smorkster <smorkster@gmail.com>
2020-09-01 08:00:29 +0200

    Update StartProcess.ps1
    Updates for the new help topic functionality

--- Script/Computer/Processes/StartProcess.ps1
+++ Script/Computer/Processes/StartProcess.ps1
@@ -1,13 +1,16 @@
-#Description = Start application on remote computer
+<#
+.Synopsis Start application on remote computer
+.Description Start application on remote computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 $Program = Read-Host "Name application to be started"
 
 Invoke-Command -ComputerName $ComputerName -Scriptblock { start $Using:Program }
 
 WriteLog -LogText "$CaseNr $ComputerName $Program"
-
 EndScript
