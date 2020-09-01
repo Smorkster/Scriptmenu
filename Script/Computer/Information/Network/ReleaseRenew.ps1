7db3e2d03a5defd9f32c490291d988dd8756a476
Smorkster <smorkster@gmail.com>
2020-09-01 07:37:57 +0200

    Update ReleaseRenew.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/Network/ReleaseRenew.ps1
+++ Script/Computer/Information/Network/ReleaseRenew.ps1
@@ -1,11 +1,14 @@
-#Description = Release remote computers IP-address and request new
+<#
+.Synopsis Release remote computers IP-address and requests new
+.Description Release remote computers IP-address and request new.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 Invoke-Command -Computername $ComputerName -Scriptblock { ipconfig /release | ipconfig /renew }
 
 WriteLog -LogText "$CaseNr $ComputerName"
-
 EndScript
