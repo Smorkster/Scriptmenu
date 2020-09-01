28f06a59cb84c208698d10be50f07b388a38bdfe
Smorkster <smorkster@gmail.com>
2020-09-01 08:11:11 +0200

    Update OpenWeb.ps1
    Updates for the new help topic functionality

--- Script/Computer/Processes/Start/OpenWeb.ps1
+++ Script/Computer/Processes/Start/OpenWeb.ps1
@@ -1,13 +1,15 @@
-#Description = Open webpage on remote computer
+<#
+.Synopsis Open webpage on remote computer
+.Description Open webpage on remote computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
 $Address = Read-Host "Write webaddress to be opened"
 
 Invoke-Command -ComputerName $ComputerName -Scriptblock ` { start $Using:Address }
 
 WriteLog -LogText "$CaseNr $ComputerName > $Adress"
-
 EndScript
