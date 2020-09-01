a19d50070f2e6c4a0f07af98b9e7ba8c76049f71
Smorkster <smorkster@gmail.com>
2020-09-01 07:24:55 +0200

    Update SystemInfo.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/SystemInfo.ps1
+++ Script/Computer/Information/SystemInfo.ps1
@@ -1,13 +1,17 @@
-#Description = Show systeminformation for remoter computer
+<#
+.Synopsis Show systeminformation for remote computer
+.Description Show system information for given computer, such as operatingsystem, date of installation, installed hotfixes etc.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 $info = systeminfo /s $ComputerName
 
 $info
 $outputFile = WriteOutput -Output $info
-WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 
+WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 EndScript
