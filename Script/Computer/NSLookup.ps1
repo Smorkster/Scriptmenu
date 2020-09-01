c7744e351d36e2cd1b84ef1109b11c87be57def8
Smorkster <smorkster@gmail.com>
2020-09-01 06:47:29 +0200

    Update NSLookup.ps1
    Updates for the new help topic functionality

--- Script/Computer/NSLookup.ps1
+++ Script/Computer/NSLookup.ps1
@@ -1,10 +1,15 @@
-#Description = Translate IP-address to computername
+<#
+.Synopsis Translates an IP-address to a computername
+.Description Searches for the IP-address, and lists the unit currently using it.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $CaseNr = Read-Host "Casenumber (if any) "
 $Destination = Read-Host "IP-address to translate"
 
 $Unit = nslookup $Destination
-WriteLog -LogText "$CaseNr $Destination > $Unit"
+$Unit | Out-Host
 
+WriteLog -LogText "$CaseNr $Destination > $Unit"
 EndScript
