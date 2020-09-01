f6c62445801c04344e0dd241134caa377951ffb7
Smorkster <smorkster@gmail.com>
2020-09-01 07:28:11 +0200

    Update IPConfig.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/Network/IPConfig.ps1
+++ Script/Computer/Information/Network/IPConfig.ps1
@@ -1,13 +1,18 @@
-#Description = Show IP configuration on remote computer
+<#
+.Synopsis Show IP configuration on remote computer
+.Description Runs command 'ipconfig /all' on given computer. The Information is then listed in consolewindow.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
-$CaseNr = Read-Host "Related casenumber (if any) "
 $ComputerName = $args[1]
+$CaseNr = Read-Host "Related casenumber (if any) "
+
 $conf = Invoke-Command -Computername $ComputerName -Scriptblock { ipconfig /all }
 
 $outputFile = WriteOutput -Output $conf
 
-WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 Start-Process notepad $outputFile -Wait
 
+WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 EndScript
