757ea6f1e569923970add25e3d7357eb234e24e8
Smorkster <smorkster@gmail.com>
2020-09-01 07:39:16 +0200

    Update Tracert.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/Network/Tracert.ps1
+++ Script/Computer/Information/Network/Tracert.ps1
@@ -1,16 +1,20 @@
-#Description = Show networkrouting from remote computer to given address
+<#
+.Synopsis Show networkrouting from remote computer to given address
+.Description Show networkrouting from remote computer to given address.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 $Destination = Read-Host "Target IP-address"
 
 $Trace = Invoke-Command -Computername $ComputerName -Scriptblock { tracert $Destination }
 
 $outputFile = WriteOutput -Output $Trace
 
-WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 Start-Process notepad $outputFile -Wait
 
+WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 EndScript
