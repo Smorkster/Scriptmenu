b92116e6215513a98ae9c6a9bed1806ab95735ce
Smorkster <smorkster@gmail.com>
2020-09-01 08:30:14 +0200

    Update Get-ADGroupMembers.ps1
    Updates for the new help topic functionality

--- Script/Group/Get-ADGroupMembers.ps1
+++ Script/Group/Get-ADGroupMembers.ps1
@@ -1,4 +1,8 @@
-#Description = List all AD-objects that are members of orggroup (Users, computers, printers etc)
+<#
+.Synopsis List all AD-objects that are members of AD-group
+.Description List all AD-objects, users/groups/computers etc, that are members of given AD-group.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 Write-Host "Listing alla members (users, computers, printers etc)`n"
@@ -9,7 +13,5 @@ $Input = Read-Host "Groupname to search for "
 Get-ADGroupMember -Identity $Input | % { $members += "$_.ObjectClass`t$_.Name`r`n" }
 
 WriteOutput -Output "Group $Input:`r`n$members"
-
 WriteLog -LogText "$CaseNr $Input`r`n`t$outputFile"
-
 EndScript
