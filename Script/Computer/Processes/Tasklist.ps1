829fc82887d1ba90885f18ef378e3e4b709ca179
Smorkster <smorkster@gmail.com>
2020-09-01 08:03:12 +0200

    Update Tasklist.ps1
    Updates for the new help topic functionality

--- Script/Computer/Processes/Tasklist.ps1
+++ Script/Computer/Processes/Tasklist.ps1
@@ -1,13 +1,17 @@
-#Description = Show running processes on remote computer
+<#
+.Synopsis Show running processes on remote computer
+.Description Show running processes on remote computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 $apps = tasklist /NH /s $ComputerName | sort
 $apps
 
 $outputFile = WriteOutput -Output $apps
-WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 
+WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 EndScript
