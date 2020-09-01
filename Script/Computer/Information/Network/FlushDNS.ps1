405abc880aeacaa0cbd05953c4ab42dc079e997c
Smorkster <smorkster@gmail.com>
2020-09-01 07:26:16 +0200

    Update FlushDNS.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/Network/FlushDNS.ps1
+++ Script/Computer/Information/Network/FlushDNS.ps1
@@ -1,12 +1,16 @@
-#Description = Clear local DNS cache on remote computer
+<#
+.Synopsis Clear local DNS cache on remote computer
+.Description Clear local DNS cache on given computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 Invoke-Command -Computername $ComputerName -Scriptblock { ipconfig /flushdns }
 
-WriteLog -LogText "$CaseNr $ComputerName"
 Write-Host "DNS cache cleared"
 
+WriteLog -LogText "$CaseNr $ComputerName"
 EndScript
