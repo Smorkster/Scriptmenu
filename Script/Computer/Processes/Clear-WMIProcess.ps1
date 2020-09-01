f54b91ad0b640b58d5e0fab57f29a79bcd7c3cca
Smorkster <smorkster@gmail.com>
2020-09-01 07:55:13 +0200

    Update Clear-WMIProcess.ps1
    Updates for the new help topic functionality

--- Script/Computer/Processes/Clear-WMIProcess.ps1
+++ Script/Computer/Processes/Clear-WMIProcess.ps1
@@ -1,9 +1,13 @@
-#Description = Helps handle 'Waiting for userlogin'
+<#
+.Synopsis Helps handle 'Waiting for userlogin'
+.Description Clears tasklist for CCMEXEC. This may help errormessage "Waiting for userlogin".
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 Invoke-Command -ComputerName $ComputerName -ScriptBlock `
 {
 	$CITask = Get-WmiObject -Query "select * from CCM_CITask where TaskState != ' PendingSoftReboot' AND TaskState != 'PendingHardReboot' AND TaskState != 'InProgress'" -Namespace root\ccm\CITasks
@@ -23,6 +27,6 @@ Invoke-Command -ComputerName $ComputerName -ScriptBlock `
 }
 
 Write-Host "You can now ask the user to reboot the computer."
-WriteLog -LogText "$CaseNr $ComputerName"
 
+WriteLog -LogText "$CaseNr $ComputerName"
 EndScript
