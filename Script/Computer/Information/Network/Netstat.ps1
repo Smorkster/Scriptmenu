61593d396541c95c3b5f90ea2f5e60df0d8d22d3
Smorkster <smorkster@gmail.com>
2020-09-01 07:32:55 +0200

    Update Netstat.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/Network/Netstat.ps1
+++ Script/Computer/Information/Network/Netstat.ps1
@@ -1,14 +1,18 @@
-#Description = Show active networkconnections on remote computer, and related applications
+<#
+.Synopsis Show active networkconnections on remote computer, and related applications
+.Description List all active networkconnections on given computer. The outputlist shows what processes owns the connection, where it is connected and if it is active.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
-$CaseNr = Read-Host "Related casenumber (if any) "
 $ComputerName = $args[1]
+$CaseNr = Read-Host "Related casenumber (if any) "
 
 $stats = Invoke-Command -Computername $ComputerName -Scriptblock { netstat -b -f }
 
 $outputFile = WriteOutput -Output $stats
 
-WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 Start-Process notepad $outputFile -Wait
 
+WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
 EndScript
