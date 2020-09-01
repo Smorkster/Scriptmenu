4c7904aa31bb3a1bf8b813395aed2d4772488dce
Smorkster <smorkster@gmail.com>
2020-09-01 08:18:23 +0200

    Update ShutdownComputer.ps1
    Updates for the new help topic functionality

--- Script/Computer/Reboot/ShutdownComputer.ps1
+++ Script/Computer/Reboot/ShutdownComputer.ps1
@@ -1,10 +1,14 @@
-#Description = Turn off remote computer
+<#
+.Synopsis Turn off remote computer
+.Description Forces a shutdown of given computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
-Stop-Computer -ComputerName $ComputerName -Force
-WriteLog -LogText "$CaseNr $ComputerName"
 
+Stop-Computer -ComputerName $ComputerName -Force
+
+WriteLog -LogText "$CaseNr $ComputerName"
 EndScript
