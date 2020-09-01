df152d12f47c373b13bc49f82de78284a45ecf89
Smorkster <smorkster@gmail.com>
2020-09-01 07:44:00 +0200

    Update LoginAsAdmin.ps1
    Updates for the new help topic functionality

--- Script/Computer/Login/LoginAsAdmin.ps1
+++ Script/Computer/Login/LoginAsAdmin.ps1
@@ -1,10 +1,14 @@
-#Description = Log on to remote computer as admin
+<#
+.Synopsis Log on to remote computer as admin
+.Description Starts a remote connection to computer, loggin in as administrator.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
 $CaseNr = Read-Host "Related casenumber (if any) "
 
 Start-Process -Filepath "C:\Windows\System32\mstsc.exe" -ArgumentList "/v:$ComputerName /f"
-WriteLog -LogText "$CaseNr $ComputerName"
 
+WriteLog -LogText "$CaseNr $ComputerName"
 EndScript
