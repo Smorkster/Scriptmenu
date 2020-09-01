77f2028dba253e4833d536a2076fdfc1ddf93454
Smorkster <smorkster@gmail.com>
2020-09-01 07:21:37 +0200

    Update RemotePCDisc.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/RemotePCDisc.ps1
+++ Script/Computer/Information/RemotePCDisc.ps1
@@ -1,11 +1,14 @@
-#Description = Show C: on remote computer
+<#
+.Synopsis Show C:\ on remote computer
+.Description Opens Explorer C:\ for the given computer opened.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 Start-Process -Filepath "C:\Windows\explorer.exe" -ArgumentList "\\$ComputerName\C$"
 
 WriteLog -LogText "$CaseNr $ComputerName"
-
 EndScript
