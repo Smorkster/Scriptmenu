bd150ab80757a2476989837446f896c814aa1aa6
Smorkster <smorkster@gmail.com>
2020-09-01 08:20:08 +0200

    Update PCManager.ps1
    Updates for the new help topic functionality

--- Script/Computer/Services/PCManager.ps1
+++ Script/Computer/Services/PCManager.ps1
@@ -1,11 +1,14 @@
-#Description = Open computermanager on remote computer
+<#
+.Synopsis Open computermanager on remote computer
+.Description Open Windows computermanager, connected to given computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 C:\Windows\System32\Compmgmt.msc -a /computer=$ComputerName
 
 WriteLog -LogText "$CaseNr $ComputerName"
-
 EndScript
