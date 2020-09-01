05e2be52eaa8e877edc7bd54f5fc82459a0933df
Smorkster <smorkster@gmail.com>
2020-09-01 08:07:12 +0200

    Update CMRepair.ps1
    Updates for the new help topic functionality

--- Script/Computer/Processes/Policys/CMRepair.ps1
+++ Script/Computer/Processes/Policys/CMRepair.ps1
@@ -1,11 +1,14 @@
-#Description = Repair CM agent on remote computer
+<#
+.Synopsis Repair CM agent on remote computer
+.Description Repair CM agent on given computer.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 Invoke-WmiMethod -ComputerName $ComputerName -Namespace root\ccm -Class sms_client -Name RepairClient
 
 WriteLog -LogText "$CaseNr $( $ComputerName.ToUpper() )"
-
 EndScript
