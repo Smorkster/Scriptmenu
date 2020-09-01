6ea2473ab8666f5b9e6ede97cdf71d24c826f687
Smorkster <smorkster@gmail.com>
2020-09-01 07:36:49 +0200

    Update Pathping.ps1
    Updates for the new help topic functionality

--- Script/Computer/Information/Network/Pathping.ps1
+++ Script/Computer/Information/Network/Pathping.ps1
@@ -1,9 +1,13 @@
-#Description = Show networkrouting from remote computer to given address
+<#
+.Synopsis Show networkrouting from remote computer to given address
+.Description Shows the path for networkconnection from given computer, to given address. Will also ping the node at each stop.
+#>
+
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
 
 $ComputerName = $args[1]
-
 $CaseNr = Read-Host "Related casenumber (if any) "
+
 $Destination = Read-Host "Target IP-address"
 
 $Route = Invoke-Command -Computername $ComputerName -Scriptblock { pathping $Destination }
@@ -12,5 +16,4 @@ $Route
 $outputFile = WriteOutput -Output $Route -FileNameAddition $ComputerName
 
 WriteLog -LogText "$CaseNr $ComputerName`r`n`t$outputFile"
-
 EndScript
