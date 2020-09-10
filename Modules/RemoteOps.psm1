cbc8471e58800866698b2133bd4a78c6dcf86d3e
Smorkster <smorkster@gmail.com>
2020-09-10 11:41:19 +0200

    Update RemoteOps.psm1
    New function "SendToast" to send a toastmessage to designated computer

--- Modules/RemoteOps.psm1
+++ Modules/RemoteOps.psm1
@@ -33,6 +33,38 @@ function RunCycle
 	}
 }
 
+############################################
+# Send a toastmessage to designated computer
+function SendToast
+{
+	param ( $Message , $ComputerName )
+	$code = {
+		$XmlString = @"
+<toast duration="Long">
+	<visual>
+		<binding template="ToastGeneric">
+			<text>Message from Servicedesk</text>
+			<text>$Message</text>
+		</binding>
+	</visual>
+	<audio src="ms-winsoundevent:Notification.Default" />
+</toast>
+"@
+
+		$AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+		$null = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
+		$null = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
+
+		$ToastXml = [Windows.Data.Xml.Dom.XmlDocument]::new()
+		$ToastXml.LoadXml($XmlString)
+
+		$Toast = [Windows.UI.Notifications.ToastNotification]::new( $ToastXml )
+		[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier( $AppId ).Show( $Toast )
+	}
+
+	Invoke-Command -ComputerName $ComputerName -ScriptBlock $code
+}
+
 $nudate = Get-Date -Format "yyyy-MM-dd HH:mm"
 $RootDir = ( Get-Item $PSCommandPath ).Directory.Parent.FullName
 $CallingScript = ( Get-Item $MyInvocation.PSCommandPath ).BaseName
