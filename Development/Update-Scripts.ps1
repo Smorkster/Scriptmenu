b980e2767717aaae76f11ab90c0913afb29b03cc
nisse smorkster <smorkster@outlook.com>
2021-02-03 13:25:15 +0100

    2021-02 Added localization New functionality to backup production-file

--- Development/Update-Scripts.ps1
+++ Development/Update-Scripts.ps1
@@ -5,17 +5,14 @@ function CheckForUpdates
 {
 	$btnUpdateScripts.IsEnabled = $false
 	$dirExclusion = @( "ErrorLogs",
-	
 						"Input",
 						"Logs",
-						"Output" )
+						"Output",
+						"UpdateRollback" )
 	$fileExclusion = @( ( Get-Item $PSCommandPath ).Name )
-	
-	
-	
-	
 	$updatedFiles.Clear()
 	$spUpdateList.Children.Clear()
+	$spOtherUpdates.Children.Clear()
 
 	$devFiles = Get-ChildItem $devRoot -Directory -Exclude $dirExclusion | Get-ChildItem -File -Recurse -Exclude $fileExclusion
 	$prodFiles = Get-ChildItem $prodRoot -Directory -Exclude $( $dirExclusion += "Development"; $dirExclusion ) | Get-ChildItem -File -Recurse
@@ -25,6 +22,7 @@ function CheckForUpdates
 	foreach ( $devFile in $devFiles )
 	{
 		$prodFile = $prodFiles | where { $_.Name -eq $devFile.Name }
+
 		if ( $prodFile -eq $null )
 		{
 			$updatedFiles.Add( @( $devFile, "" ) )
@@ -51,7 +49,7 @@ function CheckForUpdates
 	else
 	{
 		$btnUpdateScripts.IsEnabled = $false
-		$lblUpdateInfo.Content = "No updated files"
+		$lblUpdateInfo.Content = $IntmsgTable.StrNoUpdates
 	}
 
 	if ( $filesUpdatedInProd.Count -gt 0 )
@@ -74,7 +72,7 @@ function CheckAll
 function AddControlsForUpdatedFiles
 {
 	$cbMarkAll = New-Object System.Windows.Controls.CheckBox
-	$cbMarkAll.Content = "Mark all"
+	$cbMarkAll.Content = $IntmsgTable.StrSelectAll
 	$cbMarkAll.Margin = "0,0,0,10"
 	$cbMarkAll.Add_Checked( { CheckAll $true } )
 	$cbMarkAll.Add_UnChecked( { CheckAll $false } )
@@ -89,16 +87,25 @@ function AddControlsForUpdatedFiles
 		$cbUpdate.Name = ( $update[0].Name.Split( "\." ) )[0] -replace "-" -replace " "
 		$cbUpdate.Tag = "$( $update[0].FullName )`n$( $update[1].FullName )"
 		$devUpdated = Get-Date $update[0].LastWriteTime -Format 'yyyy-MM-dd HH:mm:ss'
-		if ( $update[1] -eq "" ) { $prodUpdated = "New script" }
+		if ( $update[1] -eq "" ) { $prodUpdated = $IntmsgTable.StrNew }
 		else { $prodUpdated = Get-Date $update[1].LastWriteTime -Format 'yyyy-MM-dd HH:mm:ss' }
-		$cbUpdate.ToolTip = "dev updated:`t$devUpdated`nprod updated:`t$prodUpdated"
+		$cbUpdate.ToolTip = "$( $IntmsgTable.StrUpInDev ):`t$devUpdated`n$( $IntmsgTable.StrUpInProd ):`t$prodUpdated"
 		$cbUpdate.Content = "$( ( $( $update[0] ).FullName -split "development" )[1] )"
 
-		if ( ( Get-Content $update[0].FullName | Select-String "^.Synopsis " ) -match "under development" )
+		if ( ( Get-Content $update[0].FullName | Select-String "^.Synopsis " ) -match $IntmsgTable.ScriptContentInDev )
 		{
 			$cbUpdate.FontWeight = "Bold"
 			$cbUpdate.Foreground = "Red"
-			$cbUpdate.ToolTip = "Under development`n$( $cbUpdate.ToolTip )"
+			$cbUpdate.ToolTip = "$( $IntmsgTable.StrDevTooltip )`n$( $cbUpdate.ToolTip )"
+		}
+		if ( $update[0].Name.EndsWith( "xaml" ) )
+		{
+			if ( ( Get-ChildItem -Filter "$( $update[0].Name.Replace( "xaml", "ps1" ) )" -Recurse | Select-String ".Synopsis" ) -match $IntmsgTable.ScriptContentInDev )
+			{
+				$cbUpdate.FontWeight = "Bold"
+				$cbUpdate.Foreground = "Red"
+				$cbUpdate.ToolTip = "$( $IntmsgTable.StrScriptInDevTooltip )`n$( $cbUpdate.ToolTip )"
+			}
 		}
 		$cbUpdate.VerticalContentAlignment = "Center"
 		$cbUpdate.Add_Checked( { $this.Foreground = "Green"; CheckChecked } )
@@ -106,10 +113,10 @@ function AddControlsForUpdatedFiles
 
 		$spUpdateList.AddChild( $cbUpdate )
 	}
-	$lblUpdateInfo.Content = "$( ( $spUpdateList.Children | where { $_.IsEnabled -and ( $_.Tag -ne $null ) } ).Count ) files updated"
+	$lblUpdateInfo.Content = "$( ( $spUpdateList.Children | where { $_.IsEnabled -and ( $_.Tag -ne $null ) } ).Count ) $( $IntmsgTable.StrSummaryUp )"
 	if ( ( $spUpdateList.Children | where { -not ( $_.IsEnabled ) -and ( $_.Tag -ne $null ) } ).Count -gt 0 )
 	{
-		$lblUpdateInfo.Content += "`n$( ( $spUpdateList.Children | where { -not ( $_.IsEnabled ) -and ( $_.Tag -ne $null ) } ).Count ) under development"
+		$lblUpdateInfo.Content += "`n$( ( $spUpdateList.Children | where { -not ( $_.IsEnabled ) -and ( $_.Tag -ne $null ) } ).Count ) $( $IntmsgTable.StrSummaryDev )"
 	}
 }
 
@@ -118,12 +125,12 @@ function AddControlsForUpdatedFiles
 function AddControlsForUpdatedProdFiles
 {
 	$l = New-Object System.Windows.Controls.Label
-	$l.Content = "Dessa har uppdaterats i produktion, men inte i dev."
+	$l.Content = $IntmsgTable.StrSummaryUpInProd
 	$spOtherUpdates.AddChild( $l )
 	foreach ( $file in $filesUpdatedInProd )
 	{
 		$l = New-Object System.Windows.Controls.Label
-		$l.Content = "$( $file[1].Name )`n`t$( $file[1].LastWriteTime.ToString() ) in prod.`n`t$( $file[0].LastWriteTime.ToString() ) in dev."
+		$l.Content = "$( $file[1].Name )`n`t$( $file[1].LastWriteTime.ToString() ) $( $IntmsgTable.StrUpInProd ).`n`t$( $file[0].LastWriteTime.ToString() ) $( $IntmsgTable.StrUpInDev )."
 		$l.Tag = $file
 		$l.Add_MouseRightButtonDown( { $this.Tag | foreach { & 'C:\Program Files (x86)\Notepad++\notepad++.exe' $_.FullName } } )
 		$spOtherUpdates.AddChild( $l )
@@ -138,7 +145,7 @@ function CheckChecked
 	if ( ( $spUpdateList.Children | where { $_.IsChecked -and ( $_.Tag -ne $null ) } ).Count -gt 0 )
 	{
 		$btnUpdateScripts.IsEnabled = $true
-		$lblInfo.Content = "Only update files you know that you have changed yourself"
+		$lblInfo.Content = $IntmsgTable.StrUpdateWarning
 	}
 	else
 	{
@@ -160,8 +167,19 @@ function UpdateScripts
 		if ( $fileCheckbox.Tag.Split( "`n" )[1] -eq $null ) { $updatedFileDestination = $fileCheckbox.Tag.Split( "`n" )[1] }
 		else { $updatedFileDestination = $prodRoot + "\" + ( $updatedFile.FullName.Replace( "$devRoot\", "" ) ) }
 
-		if ( Test-Path $updatedFileDestination ) { Copy-Item -Path $updatedFile.FullName -Destination $updatedFileDestination -Force }
-		else { New-Item -Path $updatedFileDestination -Value ( Get-Content $updatedFile.FullName ) -ItemType File -Force | Out-Null }
+		$OFS = "`n"
+		if ( Test-Path $updatedFileDestination )
+		{
+			$name = $updatedFileDestination -split "\\" | select -Last 1
+			$updated = Get-Date ( Get-Item $updatedFileDestination ).LastWriteTime -Format "yyyy-MM-dd HH.mm.ss"
+			$extension = ( Get-Item $updatedFileDestination ).Extension
+			$OFS = "`n"
+			$OutputEncoding = ( New-Object System.Text.UnicodeEncoding $False, $False ).psobject.BaseObject
+			New-Item -Path "$rollbackRoot\$( ( Get-Date ).Year )\$( ( Get-Date ).Month )\" -Name "$name ($( $IntmsgTable.StrRollbackName ) $updated)$extension" -ItemType File -Value ( [string]( Get-Content -Path $updatedFileDestination -Encoding UTF8 ) ) -Force
+			#Copy-Item -Path $updatedFileDestination -Destination "$rollbackRoot\$( ( Get-Date ).ToShortDateString() )\$( $updatedFileDestination -split "\\" | select -Last 1 )" -Force
+			Copy-Item -Path $updatedFile.FullName -Destination $updatedFileDestination -Force
+		}
+		else { New-Item -Path $updatedFileDestination -Value ( [string]( Get-Content $updatedFile.FullName ) ) -ItemType File -Force | Out-Null }
 
 		$spUpdateList.Children.Remove( $fileCheckbox )
 		$loop++
@@ -169,17 +187,17 @@ function UpdateScripts
 
 	$ofs = ", "
 	$nudate = Get-Date -Format "yyyy-MM-dd HH:mm"
-	$LogText = "Updating $( [string]( $fileCheckboxes.Content ) )"
+	$LogText = "$( $IntmsgTable.StrLogIntro ) $( [string]( $fileCheckboxes.Content ) )"
 	if ( @( $Script.filesUpdatedInProd ).Count -gt 0 )
 	{
-		$LogText += "`n`tFiles updated in prod, but not in dev: "
+		$LogText += "`n`t$( $IntmsgTable.StrSummaryUpInProd ): "
 		$LogText += [string]( $Script:filesUpdatedInProd | foreach { ( $_[0].FullName -split "development" )[1] } )
 	}
 	$LogFilePath = "$( ( Get-Item $PSCommandPath ).Directory.Parent.FullName )\Logs\$( [datetime]::Now.Year )\$( [datetime]::Now.Month )\Update-Scripts - log.txt"
 	if ( -not ( Test-Path $LogFilePath ) ) { New-Item -Path $LogFilePath -ItemType File -Force | Out-Null } # If logfile does not exist, create it
 	Add-Content -Path $LogFilePath -Value ( $nudate + " " + $env:USERNAME + " [" + $env:USERDOMAIN + "] => " + $LogText )
 
-	$lblUpdateInfo.Content = "$( $spUpdateList.Children.Count ) files updated"
+	$lblUpdateInfo.Content = "$( $spUpdateList.Children.Count ) $( $IntmsgTable.StrSummaryUp )"
 	CheckChecked
 	if ( $spUpdateList.Children.Count -eq 1 )
 	{ $spUpdateList.Children.Clear() }
@@ -194,12 +212,16 @@ function UpdateScripts
 
 ######################### Script start
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
+Import-Module "$( $args[0] )\Modules\GUIOps.psm1" -Force
+$culture = "sv-SE"
+Import-LocalizedData -BindingVariable IntmsgTable -UICulture $culture -FileName "$( ( $PSCommandPath.Split( "\" ) | select -Last 1 ).Split( "." )[0] ).psd1" -BaseDirectory "..\Localization"
 
 $Window, $vars = CreateWindow
 $vars | foreach { Set-Variable -Name $_ -Value $Window.FindName( $_ ) }
 
 $Script:devRoot = ( Get-Item $PSCommandPath ).Directory.FullName
 $Script:prodRoot = ( Get-Item $PSCommandPath ).Directory.Parent.FullName
+$Script:rollbackRoot = "$( ( Get-Item $PSCommandPath ).Directory.Parent.FullName )\UpdateRollback"
 $Script:updatedFiles = New-Object System.Collections.ArrayList
 $Script:filesUpdatedInProd = New-Object System.Collections.ArrayList
 $btnCheckForUpdates.Add_Click( { CheckForUpdates } )
---
+++ Localization/sv-SE/Update-Scripts.psd1
@@ -0,0 +1,16 @@
+ConvertFrom-StringData @'
+StrNoUpdates = Inga uppdaterade filer
+StrSelectAll = Markera alla
+StrUpInDev = dev uppdaterad
+StrUpInProd = prod uppdaterad
+ScriptContentInDev = under utveckling
+StrDevTooltip = Under utveckling
+StrScriptInDevTooltip = Skriptfil under utveckling
+StrSummaryUp = filer uppdaterade
+StrSummaryDev = under utveckling
+StrSummaryUpInProd = Dessa har uppdaterats i produktion, men inte i dev.
+StrUpdateWarning = Uppdatera enbart de filer du vet att du ändrat själv
+StrRollbackName = uppdaterad
+StrLogIntro = Uppdaterar
+StrNew = Nytt skript
+'@
