4b4525fb56739aec23653f756676c06736ffa7df
nisse smorkster <smorkster@outlook.com>
2021-02-17 09:02:31 +0100

    210217 Added localization

--- Script/Folder/Get-GFolderPermissions.ps1
+++ Script/Folder/Get-GFolderPermissions.ps1
@@ -3,16 +3,15 @@
 .Description For given shared folders, list its owner and users with permission for it.
 #>
 
+Import-Module "$( $args[0] )\Modules\ConsoleOps.psm1" -Force
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force
-
-$CaseNr = Read-Host "Related casenumber (if any) "
 
 do
 {
-	$Customer = Read-Host "Name customer ( Org1, Org2, Org3 )"
-} until ( "Org1", "Org2", "Org3" -contains $Customer )
+	$Customer = Read-Host $msgTable.QCustomer
+} until ( $msgTable.CodeCustomerList -match $Customer )
 
-Write-Host "Past a list of folders to list permissions for`nFull pathway or only foldername: (Press Enter twice to finish)"
+Write-Host $msgTable.QFolders
 $FoldersIn = GetConsolePasteInput -Folders
 
 $FailedFolders = @()
@@ -24,7 +23,7 @@ foreach ( $Folder in $FoldersIn )
 
 	if ( -not ( Test-Path $Folder ) )
 	{
-		Write-Host "Pathway '$Folder' couldn't be found! Verify name and run again."
+		Write-Host "'$Folder' $( $msgTable.StrNotFound )"
 		$FailedFolders += $Folder
 	}
 	else
@@ -33,15 +32,15 @@ foreach ( $Folder in $FoldersIn )
 	}
 }
 
-Write-Host "Geting information and writes to file"
+Write-Host $msgTable.StrSearching
 
 $output = @()
 $output += "------------------------------------------------------------------------------"
-$output += "Listin permissions for these folders`r`n"
+$output += "$( $msgTable.StrOutTitle )`r`n"
 $output += $Folders
 if ( $FailedFolders.Count -gt 0 )
 {
-	$output += "`r`nThese folders couldn't be found, and will not be listed:`r`n"
+	$output += "`r`n$( $msgTable.StrOutNotFoundTitle )`r`n"
 	$output += $FailedFolders
 }
 $output += "`r`n------------------------------------------------------------------------------"
@@ -50,34 +49,29 @@ Start-Sleep 3
 
 foreach ( $Folder in $Folders )
 {
-	switch ( $Folder.Substring( 3, 3 ) )
-	{
-		"Org1" { $GroupPrefix = "Org1_Fil_AdOrg1_Grp_" }
-		"Org2" { $GroupPrefix = "Org2_Fil_AdOrg2_Grp_" }
-		"Org3" { $GroupPrefix = "Org3_Fil_AdOrg3_Grp_" }
-	}
+	$GroupPrefix = ( Invoke-Expression $msgTable.CodeGP ).( $Folder.Substring( 3, 3 ) )
 	$FolderName = $Folder.Substring( 7 )
 	$output += "`r`n************************`r`n$Folder`r`n"
-	$Owner = Get-ADGroup ( ( $GroupPrefix + $FolderName + "_User_C" ) -replace "å", "a" -replace "ä", "a" -replace "ö", "o" -replace " ", "_" -replace "é", "e" ) -Properties ManagedBy | Select-Object -ExpandProperty Managedby
+	$Owner = Get-ADGroup ( ( $GroupPrefix + $FolderName + $msgTable.StrGrpNameSuffixWrite ) -replace "å", "a" -replace "ä", "a" -replace "ö", "o" -replace " ", "_" -replace "é", "e" ) -Properties ManagedBy | Select-Object -ExpandProperty Managedby
 
 	if ( $null -ne $Owner )
 	{
-		$output += "Owner: " + ( Get-ADUser $Owner | Select-Object -ExpandProperty Name )
+		$output += "$( $msgTable.StrOwner ) $( ( Get-ADUser $Owner ).Name )"
 	}
 	else
 	{
-		$output += "Owner is missing"
+		$output += $msgTable.StrOwnerMissing
 	}
 
-	$output += "`r`nRead-permission: "
+	$output += "`r`n$( $msgTable.StrReadPerm ) "
 
-	$RGroups = Get-ADGroupMember ( ( $GroupPrefix + $FolderName + "_User_R" ) -replace "å", "a" -replace "ä", "a" -replace "ö", "o" -replace " ", "_" -replace "é", "e" ) | Select-Object -ExpandProperty Name
+	$RGroups = Get-ADGroupMember ( ( $GroupPrefix + $FolderName + $msgTable.StrGrpNameSuffixRead ) -replace "å", "a" -replace "ä", "a" -replace "ö", "o" -replace " ", "_" -replace "é", "e" ) | Select-Object -ExpandProperty Name
 	$ROrgGroups = $RGroups | Where-Object { $_ -like ( $Customer + "_org_*" ) }
 	$RGroups = $RGroups | Where-Object { $_ -notlike ( $Customer + "_org_*" ) }
 
 	if ( $RGroups.Count -eq 0 )
 	{
-		$output += "<No read permissions>"
+		$output += $msgTable.StrNoRead
 	}
 	else
 	{
@@ -86,24 +80,21 @@ foreach ( $Folder in $Folders )
 
 	if ( $null -ne $ROrgGroups )
 	{
-		foreach ( $ROrgGroup in $ROrgGroups )
+		foreach ( $OrgGroup in $ROrgGroups )
 		{
-			$ROrgGroupID = ( Get-ADGroup $ROrgGroup -Properties * | Select-Object -ExpandProperty "orgIdentity" )
+			$OrgGroupID = ( Get-ADGroup $OrgGroup -Properties * | Select-Object -ExpandProperty $msgTable.StrAdIdPropPrefix ) -replace $msgTable.StrAdIdPrefix, ""
 
-			switch ( $ROrgGroup )
+			if ( -not ( $OrgGroupMembers = ( Invoke-Expression $msgTable.CodeOrgGrpMembers ) ) )
 			{
-				"Org_1_Users" { $ROrgGroupMembers = "All users at Org1 " }
-				"Org_2_Users" { $ROrgGroupMembers = "All users at Org2" }
-				"Org_3_Users" { $ROrgGroupMembers = "All users at Org3" }
-				default { $ROrgGroupMembers = Get-ADGroupMember $ROrgGroup | Select-Object -ExpandProperty Name }
+				$OrgGroupMembers = Get-ADGroupMember $OrgGroup | Select-Object -ExpandProperty Name
 			}
 
-			$output += "`r`nSynced group $ROrgGroup (Department with Id $ROrgGroupID and its subdepartments) containing these users:"
-			$output += $ROrgGroupMembers | Sort-Object
+			$output += "`r`n$( Invoke-Expression $msgTable.CodeOrgGrpMembersOutput )"
+			$output += $OrgGroupMembers | Sort-Object
 		}
 	}
 
-	$output += "`r`nWrite permission: "
+	$output += "`r`n$( $msgTable.StrWritePerm ) "
 
 	$CGroups = Get-ADGroupMember ( ( $GroupPrefix + $FolderName + "_User_C" ) -replace "å", "a" -replace "ä", "a" -replace "ö", "o" -replace " ", "_" -replace "é", "e" ) | Select-Object -ExpandProperty Name
 	$COrgGroups = $CGroups | Where-Object { $_ -like ( $Customer + "_org_*" ) }
@@ -111,7 +102,7 @@ foreach ( $Folder in $Folders )
 
 	if ( $CGroups.Count -eq 0 )
 	{
-		$output += "<No permissions>"
+		$output += $msgTable.StrNoWrite
 	}
 	else
 	{
@@ -120,30 +111,23 @@ foreach ( $Folder in $Folders )
 
 	if ( $null -ne $COrgGroups )
 	{
-		foreach ( $COrgGroup in $COrgGroups )
+		foreach ( $OrgGroup in $COrgGroups )
 		{
-			$COrgGroupID = ( Get-ADGroup $COrgGroup -Properties * | Select-Object -ExpandProperty "orgIdentity" )
+			$OrgGroupID = ( Get-ADGroup $OrgGroup -Properties * | Select-Object -ExpandProperty $msgTable.StrAdIdPropPrefix ) -replace $msgTable.StrAdIdPrefix, ""
 
-			$output += "`r`nSynced group $COrgGroup (Department with Id $COrgGroupID and its subdepartments) containint these users"
-
-			switch ( $COrgGroup )
+			if ( -not ( $OrgGroupMembers = ( Invoke-Expression $msgTable.CodeOrgGrpMembers ) ) )
 			{
-				"Org_1_Users" { $COrgGroupMembers = "All users at Org1" }
-				"Org_2_Users" { $COrgGroupMembers = "All users at Org2" }
-				"Org_3_Users" { $COrgGroupMembers = "All users at Org3" }
-				default { $COrgGroupMembers = Get-ADGroupMember $COrgGroup | Select-Object -ExpandProperty Name }
+				$OrgGroupMembers = Get-ADGroupMember $OrgGroup | Select-Object -ExpandProperty Name
 			}
-			$output += $COrgGroupMembers | Sort-Object
+			$output += "`r`n$( Invoke-Expression $msgTable.CodeOrgGrpMembersOutput )"
+			$output += $OrgGroupMembers | Sort-Object
 		}
 	}
 }
 
 $outputFile = WriteOutput -Output $output
-Write-Host "List written to '$outputFile'"
-if ( ( Read-Host "Open file? (Y/N)" ) -eq "Y" ) { Start-Process notepad }
+Write-Host "$( $msgTable.StrOutSum ) '$outputFile'"
+if ( ( Read-Host "$( $msgTable.QOpenSum ) (Y/N)" ) -eq "Y" ) { Start-Process notepad $outputFile -Wait }
 
-WriteLog -LogText "$CaseNr Summary > $outputFile"
-
-Start-Process notepad $outputFile -Wait
-
+WriteLog -LogText "$( $msgTable.StrSum ) > $outputFile" | Out-Null
 EndScript
