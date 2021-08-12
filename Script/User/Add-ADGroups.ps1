b7e7c8140dd440658637941e8f73b2acade40819
Smorkster <smorkster@gmail.com>
2021-08-12 14:23:14 +0200

    210812 Fixed logging to follow new log-formating

--- Script/User/Add-ADGroups.ps1
+++ Script/User/Add-ADGroups.ps1
@@ -5,10 +5,12 @@
 .Author Smorkster (smorkster)
 #>
 
-Import-Module "$( $args[0] )\Modules\ConsoleOps.psm1" -Force -Argumentlist $args[1]
-Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force -Argumentlist $args[1]
+Import-Module "$( $args[0] )\Modules\ConsoleOps.psm1" -Force -ArgumentList $args[1]
+Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force -ArgumentList $args[1]
 
 $User = Read-Host $msgTable.QID
+$success = $true
+
 if ( dsquery user -samid $User )
 
 {
@@ -35,14 +37,16 @@ if ( dsquery user -samid $User )
 		}
 		catch
 		{
-			WriteErrorLog -LogText $_
+			$success = $fail
 			if ( $_.Exception.Message -eq "Insufficient access rights to perform the operation" )
 			{
+				WriteErrorLogTest -LogText $_ -UserInput $group -Severity "PermissionFail"
 				$noPermission += $group
 				$t = "$( $msgTable.ErrNoPermission ) '$group'"
 			}
 			else
 			{
+				WriteErrorLogTest -LogText $_ -UserInput $group -Severity "OtherFail"
 				$other += ,@( $group, $_.Exception.Message )
 				$t = "$( $msgTable.ErrOther ) '$group':`n`t$_.Exception.Message"
 			}
@@ -65,8 +69,9 @@ else
 {
 	Write-Host "$( $msgTable.ErrNoAccount ) '$User'"
 	$logText = $msgTable.WErrMessage
+	$success = $fail
 }
 
 
-WriteLog -LogText $logText | Out-Null
+WriteLogTest -LogText $logText -UserInput $User -Success $success | Out-Null
 EndScript
