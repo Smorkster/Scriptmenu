8a3a82fffb601e289bd1b661151b58d23a442219
Smorkster <smorkster@gmail.com>
2021-09-03 14:44:45 +0200

    210903 (Get-LoggedOnUsers) Placed update in wrong folder

--- Script/Computer/Login/Get-LoggedOnUsers.ps1
+++ Script/Computer/Login/Get-LoggedOnUsers.ps1
@@ -1,17 +1,17 @@
 <#
-.Synopsis Show logged on users on remote computer
-.Description Show logged on users on given computer.
+.Synopsis View logged in users
+.Description List all logged in users on the specified computer.
 .Depends WinRM
+.State Prod
 .Author Smorkster (smorkster)
 #>
 
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force -ArgumentList $args[1]
 
 $ComputerName = $args[2]
-$CaseNr = Read-Host "Related casenumber (if any) "
 
 $Users = wmic /node:$ComputerName ComputerSystem Get UserName | Where-Object { $_ -notmatch "UserName" -and $_ -ne "" }
 $Users
 
-WriteLog -LogText "$CaseNr $( $ComputerName.ToUpper() ) > $( $Users.Trim() )"
+WriteLogTest -Text $Users.Trim() -UserInput $ComputerName -Success $true | Out-Null
 EndScript
