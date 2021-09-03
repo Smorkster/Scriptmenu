17e084d0825d43f2fd56771be304ce4fbe43c3a5
Smorkster <smorkster@gmail.com>
2021-09-03 10:44:39 +0200

    210903 (Open-ServicetagWebPage) Now uses the new logging functions in FileOps Optimized information retrieval

--- Script/Computer/Information/Open-ServicetagWebPage.ps1
+++ Script/Computer/Information/Open-ServicetagWebPage.ps1
@@ -8,21 +8,17 @@
 Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force -ArgumentList $args[1]
 
 $ComputerName = $args[2]
+$CimComputer = Get-CimInstance -ClassName CIM_Chassis -ComputerName $ComputerName
 
-# Get remote servicetag.
-$Vendor = wmic /node:$ComputerName csproduct get vendor
-$Servicetag = (wmic /node:$ComputerName csproduct get identifyingnumber)[2].Trim()
+# Get remote servicetag
+$Vendor = $CimComputer.Manufacturer
+$Servicetag = $CimComputer.SerialNumber
 
 # Open Google Chrome with manufacturer webpage for servicetag
-if ( $Vendor -match "Dell" )
-{
-	$adress = "http://www.dell.com/support/my-support/se/sv/sebsdt1/product-support/servicetag/$Servicetag"
-}
-elseif ( $Vendor -match "Lenovo")
-{
-	$adress = "https://pcsupport.lenovo.com/us/en/products/$Servicetag"
-}
+if ( $Vendor -match "Dell" ) { $adress = "http://www.dell.com/support/my-support/se/sv/sebsdt1/product-support/servicetag/$Servicetag" }
+elseif ( $Vendor -match "Lenovo") { $adress = "https://pcsupport.lenovo.com/us/en/products/$Servicetag" }
+
 Start-Process chrome.exe $adress
 
-WriteLog -LogText "$ComputerName" | Out-Null
+WriteLogTest -Text $adress -UserInput $ComputerName -Success $true | Out-Null
 EndScript
