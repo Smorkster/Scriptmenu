18ca58418ac9fa6792c7706cd9584add5cd6ec54
Smorkster <smorkster@gmail.com>
2021-09-10 16:55:13 +0200

    210910 (Open-PCServices) Now uses the new logging functions in FileOps

--- Script/Computer/Services/Open-PCServices.ps1
+++ Script/Computer/Services/Open-PCServices.ps1
@@ -9,7 +9,12 @@ Import-Module "$( $args[0] )\Modules\FileOps.psm1" -Force -ArgumentList $args[1]
 
 $ComputerName = $args[2]
 
-C:\Windows\System32\services.msc -a /computer=$ComputerName
+try { C:\Windows\System32\services.msc -a /computer=$ComputerName }
+catch
+{
+	$eh = WriteErrorlogTest -LogText $_ -UserInput "." -Severity "OtherFail" -ComputerName $ComputerName
+	Write-Host $msgTable.StrErr
+}
 
-WriteLog -LogText "$ComputerName" | Out-Null
+WriteLogTest -Success ( $null -eq $eh ) -ComputerName $ComputerName -ErrorLogHash $eh | Out-Null
 EndScript
